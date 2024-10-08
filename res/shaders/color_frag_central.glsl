2a88221696e1f4218c7c37186c339785f4a92bfe
Han Dong <donghan@ecs.vuw.ac.nz>
2024-10-08 20:02:11 +1300

    shader update

--- res/shaders/color_frag_central.glsl
+++ res/shaders/color_frag_central.glsl
@@ -5,8 +5,12 @@ uniform mat4 uProjectionMatrix;
 uniform mat4 uModelViewMatrix;
 uniform vec3 uColor;
 
+// veg-cov
+uniform float uCovDensity;
+
 // viewspace data (this must match the output of the fragment shader)
 in VertexData {
+    vec3 orgPosition;
 	vec3 position;
 	vec3 normal;
 	vec2 textureCoord;
@@ -20,6 +24,10 @@ void main() {
 	vec3 eye = normalize(-f_in.position);
 	float light = abs(dot(normalize(f_in.normal), eye));
 	vec3 oColor = uColor;
+	float rnd = abs(f_in.orgPosition.x * f_in.orgPosition.y);
+	if(fract(rnd) < uCovDensity * 0.5) {
+		oColor = vec3(0, 0.8, 0);
+	}
 	vec3 color = mix(oColor / 4, oColor, light);
 
 	// output to the frambuffer
--- res/shaders/color_vert_central.glsl
+++ res/shaders/color_vert_central.glsl
@@ -5,6 +5,10 @@ uniform mat4  uProjectionMatrix;
 uniform mat4  uModelViewMatrix;
 uniform vec3  uColor;
 
+// for deformation
+uniform int   uIsDeformation;
+uniform float uDeformation;
+
 // mesh data
 layout(location = 0) in vec3 aPosition;
 layout(location = 1) in vec3 aNormal;
@@ -12,14 +16,23 @@ layout(location = 2) in vec2 aTexCoord;
 
 // model data (this must match the input of the vertex shader)
 out VertexData {
+    vec3 orgPosition;
 	vec3 position;
 	vec3 normal;
 	vec2 textureCoord;
 } v_out;
 
 void main() {
+    // deformation
+	float amplitude = uDeformation;
+	vec3 aDeformPosition = aPosition;
+	if(uIsDeformation == 1) {
+		float h = amplitude * fract(aPosition.x * 12306.2322);
+		aDeformPosition.y += h;
+	}
 	// transform vertex data to viewspace
-	v_out.position = (uModelViewMatrix * vec4(aPosition, 1)).xyz;
+	v_out.orgPosition = aPosition;
+	v_out.position = (uModelViewMatrix * vec4(aDeformPosition, 1)).xyz;
 	v_out.normal = normalize((uModelViewMatrix * vec4(aNormal, 0)).xyz);
 	v_out.textureCoord = aTexCoord;
 
